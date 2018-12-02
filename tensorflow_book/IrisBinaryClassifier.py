3d9ffe76895e07499cf98fe92989a1104a91ccec
lnc <lnc8023@163.com>
2018-12-02 16:08:04 +0800

    1.add plt

--- tensorflow_book/IrisBinaryClassifier.py
+++ tensorflow_book/IrisBinaryClassifier.py
@@ -17,38 +17,48 @@ import matplotlib.pyplot as plt
 # 1. 导入数据，将label置为0和1
 iris_data = datasets.load_iris()
 iris_labels = np.array([1. if x == 0 else 0. for x in iris_data.target]).reshape(150, 1)
-iris_features = iris_data.data
-# print(iris_features)
+iris_features = np.array([[x[0], x[3]] for x in iris_data.data])
+# iris_features = iris_data.data
+print(iris_features.shape)
 # print(iris_labels.shape)
 
 # 2. 参数声明
-batch_size = 20
-feature = tf.placeholder(shape=[None, 4], dtype=tf.float32)
-# feature_2 = tf.placeholder(shape=[None, 1], dtype=tf.float32)
-# feature_3 = tf.placeholder(shape=[None, 1], dtype=tf.float32)
-# feature_4 = tf.placeholder(shape=[None, 1], dtype=tf.float32)
-y_pre = tf.placeholder(shape=[None, 1], dtype=tf.float32)
-weight = tf.Variable(tf.random_normal(shape=[4, 1]), dtype=tf.float32)
+batch_size = 150
+feature = tf.placeholder(shape=[None, 2], dtype=tf.float32)
+y_real = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+weight = tf.Variable(tf.random_normal(shape=[2, 1]), dtype=tf.float32)
 bias = tf.Variable(tf.random_normal(shape=[1, 1]), dtype=tf.float32)
 
 # 3. 损失函数、参数优化
-y_res = tf.add(tf.matmul(feature, weight), bias)
-loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=y_res, logits=y_pre)
-op = tf.train.GradientDescentOptimizer(0.05)
+y_pre = tf.add(tf.matmul(feature, weight), bias)
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_real, logits=y_pre))
+op = tf.train.GradientDescentOptimizer(0.3)
 train = op.minimize(loss)
 
 # 4. 初始化
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
 # 5. 模型训练
-iter_num = 1000
+iter_num = 100
+loss_ = []
 for i in range(iter_num):
     rand_index = np.random.choice(len(iris_features), size=batch_size)
     feature_data = iris_features[rand_index]
     # print(feature_data)
     y_data = iris_labels[rand_index]
-    sess.run(train, feed_dict={y_pre:y_data, feature:feature_data})
-    if i % 100 == 0:
-        print('w:' + str(sess.run(tf.transpose(weight))) + '\t' + 'b:' + str(sess.run(bias)))
+    sess.run(train, feed_dict={y_real:y_data, feature:feature_data})
+    w = sess.run(tf.transpose(weight))
+    b = sess.run(bias)
+    loss_.append(sess.run(loss, feed_dict={y_real:y_data, feature:feature_data}))
+    y_prediction = tf.round(tf.nn.sigmoid(w * feature + b))
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(y_prediction, y_real), tf.float32))
+    if i % 10 == 0:
+        print('w:' + str(w) + '\t' + 'b:' + str(b))
+        print('Accuracy = ' + str(sess.run(accuracy, feed_dict={y_real:y_data, feature:feature_data})))
+
+# 6. 绘图
+plt.plot(loss_, 'g-')
+plt.title('Loss')
+plt.show()
